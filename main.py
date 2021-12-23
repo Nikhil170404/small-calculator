254d97c0ab54a230fdf9664010c417376f1abfd8
root <root@localhost>
2021-12-23 19:50:22 +0530

    auto publish 1640269221723

---
+++ main.py
@@ -0,0 +1,29 @@
+print("Mini calculator")
+
+
+first= input("enter first number;")
+operator=input("enter operator(+,-,*,/,%);")
+second=input("enter second number;")
+
+first= int(first)
+
+second= int(second)
+
+
+if operator=="+":
+  print(first+second)
+  
+elif operator=="-":
+  print(first-second)
+  
+elif operator=="*":
+  print(first*second)
+  
+elif operator=="/":
+  print(first/second)
+  
+elif operator=="%":
+  print(first%second)
+  
+else:
+  print("invalid operation")
